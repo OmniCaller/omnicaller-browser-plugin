2e217ead31581597736215b26a610712cd426248
Kai <ca2pa23187@gmail.com>
2025-02-21 23:37:23 +0800

    feat: newtab页、安装插件SOP

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Startup
 
+
+UI框架: ArcoDesign  
+pnpm  
+react  
+node: v22.11.0  
+
+
 ```
 pnpm create plasmo --with-src
 pnpm run dev
 pnpm run build
-
 ```
+
+[](./img/load-unpacked.png)
--- src/newtab.tsx
+++ src/newtab.tsx
@@ -1,23 +1,47 @@
-import { useState } from "react"
-
+import {useState} from "react"
+import {Calendar, Link, Space, Typography} from '@arco-design/web-react';
+import "@arco-design/web-react/dist/css/arco.css";
 import "./style.css"
+
+const {Title, Paragraph, Text} = Typography;
+
+const extId = chrome.runtime.id
+console.log("Extension ID:", extId)
 
 function IndexNewtab() {
   const [data, setData] = useState("")
-
+  const optionsUrl = chrome.runtime.getURL("options.html");
+  const deltaFlyerUrl = chrome.runtime.getURL("tabs/delta-flyer.html");
   return (
-    <div
-      className="new-tab"
-      style={{
-        padding: 16,
-        display: "flex",
-        flexDirection: "column"
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlasmoHQ</footer>
+    <div style={{padding: 20}}>
+      <Typography style={{marginTop: -40}}>
+        <Title>Hi, </Title>
+        <Paragraph>
+          💪️ Onz~ Om~ Onn~
+        </Paragraph>
+        <Paragraph>
+          <ol>
+            <li>simple simple simple</li>
+            <li>easy easy easy</li>
+            <li>good good good</li>
+          </ol>
+        </Paragraph>
+      </Typography>
+      <Space direction='vertical'>
+        <Link href={optionsUrl} status='success'>
+          To options
+        </Link>
+        <Link href={deltaFlyerUrl}>
+          To delta-flyer
+        </Link>
+      </Space>
+      <div style={{height: 20}}></div>
+      <Calendar
+        panel
+        panelTodayBtn
+        style={{marginRight: 50}}
+        onChange={(a) => console.log(a)}
+      />
     </div>
   )
 }
